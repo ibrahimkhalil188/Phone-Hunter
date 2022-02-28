da2eb98bbe5939467ea3ee76c93a5f1d357f6a62
unknown <ibrahimkhalil.mine@gmail.com>
2022-02-28 23:35:25 +0600

    display phone in UI

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const PhonesData = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => displayPhone(data))
+        error.innerHTML = ""
     }
 }
 
@@ -22,16 +23,21 @@ const displayPhone = phones => {
     }
     else {
         const phoneContainer = document.getElementById("phone-container")
-        console.log(phonesData)
         phonesData.forEach(phone => {
             console.log(phone)
             const div = document.createElement("div")
-            div.classList.add(col)
+            div.classList.add("col")
             div.innerHTML = `
-            
-            
+            <div class="card">
+                    <img src="${phone.image}" class=" w-75 mx-auto card-img-top" alt="...">
+                <div class="card-body mx-auto">
+                    <h3 class="card-title">${phone.phone_name}</h3>
+                    <h4 class="card-text mb-3">${phone.brand}</h4>
+                    <button type="button" class="btn btn-info fs-5">See Details</button>
+                </div>
+            </div>
             `
-
+            phoneContainer.appendChild(div)
         });
     }
 }
